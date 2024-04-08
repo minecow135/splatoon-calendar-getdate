eb44d65b055eb1dce95b4c3477e41631be9b4e62
mine_cow135 <krivig123@gmail.com>
2024-04-08 22:06:13 +0200

    add run once at start of script

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const cron = require('node-cron');
 
+// Run once at the start
+require ("./getData.js");
+require ("./createIcs.js");
+
 cron.schedule('0 45 11 * * *', () => {
     require ("./getData.js");
 });
