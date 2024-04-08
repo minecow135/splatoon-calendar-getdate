ac1c8a38d3620d14f10945e45d14212c43f99ada
mine_cow135 <krivig123@gmail.com>
2024-04-08 22:56:48 +0200

    moved db login to env variables

---
+++ db.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+sql = {
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
+}
+
+module.exports = sql;
