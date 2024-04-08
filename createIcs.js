e6555c1f6a5d4174e84c11c44112cc95b5e606eb
mine_cow135 <krivig123@gmail.com>
2024-04-08 17:51:51 +0200

    working creation of ics file

---
+++ createIcs.js
@@ -0,0 +1,48 @@
+const mysql = require('mysql2');
+const ics = require('ics');
+const { writeFileSync } = require('fs');
+
+const sql = require('./db.js');
+
+function getMonthFromString(mon){
+    return new Date(Date.parse(mon +" 1, 2012")).getMonth()+1
+}
+
+sqlconnection = mysql.createConnection(sql);
+
+sqlconnection.connect((err) => {
+    if (err) throw err;
+    console.log('MySQL connected');
+});
+
+eventType = "splatfest";
+var sqlGetCalData = 'SELECT `id`, `title`,`description`, `startDate`, `created`, `duration`, `uid`  FROM `splatCal` WHERE `event` = ?';
+sqlconnection.query(sqlGetCalData, [ eventType ], function (error, events) {
+    if (error) throw error;
+    if (events){
+        let eventArr = []
+        for (const event of events) {
+            let title = event.title
+            let description = event.description
+            let busyStatus = 'FREE'
+            let start = [ event.startDate.getFullYear(), event.startDate.getMonth(), event.startDate.getDate(), event.startDate.getHours(), event.startDate.getMinutes() ]
+            let duration = { days: event.duration }
+            let uid = event.uid
+            let created = [ event.created.getFullYear(), event.created.getMonth(), event.created.getDate(), event.created.getHours(), event.created.getMinutes() ]
+
+            eventArr.push({ title, description, busyStatus, start, duration, uid, created })
+        }
+
+        const { error, value } = ics.createEvents(eventArr)
+        if (error) {
+            console.log(error)
+            return
+        }
+
+        console.log(value)
+        
+        writeFileSync(`${__dirname}/splatfest.ics`, value)
+    } else {
+        console.log("no splatfests saved")
+    }
+});
